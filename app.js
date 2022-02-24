cde49bf00add58e06d84e61351fd3886f53d0822
blackDoGumjung <blackmisthj@gmail.com>
2022-02-24 22:10:00 +0900

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+// let exit = true
+
+// while(exit){// 무한루프
+//     console.log("hello, world!") 
+// }
+
+const moment = require('moment') // 노드 모듈에서 찾는 명령어 
+
+console.log("hello, world!")
+console.log(moment(new Date().format("YYYY-MM-DD HH:mm:ss")))
